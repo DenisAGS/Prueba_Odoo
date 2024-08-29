e5854cb6fe42da88592f401dd704a85330b37509
DenisAGS <denissearelyg@gmail.com>
2024-08-29 15:31:30 -0600

    Primer commit

--- open_academy/models/models.py
+++ open_academy/models/models.py
@@ -4,7 +4,8 @@ from odoo import _, api, fields, models, exceptions
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    company_type = fields.Selection(selection_add=[('is_school','Escuela')]) #, ('student_id', 'Estudiante')])
+    company_type = fields.Selection(selection_add=[('is_school','Escuela'), ('student_id', 'Estudiante')])
+    student_id = fields.Many2one('academia.student', 'Estudiante')
 
 class academia_student(models.Model):
     _name="academia.student"
@@ -23,14 +24,34 @@ class academia_student(models.Model):
     ], "Estado", default="draft")
     active = fields.Boolean("Activo", default=True)
     age = fields.Integer('Edad')
-    #curp = fields.Char("CURP", size=18, required=True)
+    curp = fields.Char("CURP", size=18, required=True)
     
     #Relaciones
     partner_id = fields.Many2one('res.partner', 'Escuela')
     calificaciones_id = fields.One2many('academia.calificacion', 'student_id', 'Calificaciones')
-    #country = fields.Many2one('res.country', 'Pais', related="partner_id.country_id")
+    country = fields.Many2one('res.country', 'Pais', related="partner_id.country_id")
     
-    #@api.constrains('curp')
-    #def _check_curp(self):
-    #    if len(self.curp) < 18:
-    #        raise exceptions.ValidationError('La curp debe ser de 18 digitos')
+    @api.model
+    def create(self, values):
+        if values['name']:
+            nombre = values['name']
+            if self.env['academia.student'].search([('name', '=', self.name)]):
+                values.update({
+                    'name' : values['name']+"(copy)"
+                })
+            res = super(academia_student, self).create(values)
+            partner_obj = self.env['res.partner']
+            vals_to_partner = {
+                'name': res['name']+" "+res['lastname'],
+                'company_type': "student_id",
+                'student_id': res['id'],
+            }
+            print(vals_to_partner)
+            partner = partner_obj.create(vals_to_partner)
+            print('==> partner_id ', partner)
+            return res
+    
+    @api.constrains('curp')
+    def _check_curp(self):
+        if len(self.curp) < 18:
+            raise exceptions.ValidationError('La curp debe ser de 18 digitos')
